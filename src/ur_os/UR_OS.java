b95617179c41d5cef14eb70b54e09db17f63c017
Pedro Wightman <pedro.wightman@urosario.edu.co>
2024-11-01 08:26:23 -0500

    Update UR_OS.java

--- src/ur_os/UR_OS.java
+++ src/ur_os/UR_OS.java
@@ -14,7 +14,7 @@ import ur_os.system.SimulationType;
  */
 public class UR_OS {
 
-    private static String VERSION = "0.0.5.2";
+    private static String VERSION = "0.0.5.3";
     
     /**
      * @param args the command line arguments
